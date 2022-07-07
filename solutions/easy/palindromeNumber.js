bbdfe775c5f2d56f0e7764bbb30b3909c18b5899
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-06 20:07:06 -0600

    One liner solution

--- solutions/easy/palindromeNumber.js
+++ solutions/easy/palindromeNumber.js
@@ -1,17 +1,5 @@
 const palindromeNumber = (x) => {
-    if(x < 0) return false;
-    const str = x.toString();
-    const arr1 = [];
-    const arr2 = [];
-    let i = 0;
-    let j = str.length - 1;
-    while(i < Math.floor(str.length/2)) {
-        arr1.push(str[i]);
-        arr2.push(str[j]);
-        i++;
-        j--;
-    }
-    return arr1.toString() === arr2.toString();
+    return x.toString() === x.toString().split("").reverse().join("")  
 }
 
 module.exports = { palindromeNumber };
